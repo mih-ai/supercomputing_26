9ed2e8e0d987ada90611a3caa529226cd030fd76
Manne Hannula <manneha@oamk.fi>
2026-04-08 16:17:08 +0300

    sh lisatty

---
+++ run_moi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=moi_job
+#SBATCH --output=moi_output_%j.txt
+#SBATCH --error=moi_error_%j.txt
+#SBATCH --time=00:05:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --partition=small
+
+module load python-data
+
+python moi.py
